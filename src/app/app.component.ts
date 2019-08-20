2fccd130586636d7c43583548bebb2ac08ba9440
Jamie Perkins <jamie.perkins@gmail.com>
2019-08-20 17:25:13 -0600

    date and number formatting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Inject, LOCALE_ID } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'i18n-demo';
+  title = 'i18n demo';
+
+  currentDate = new Date();
+
+  locale = 'en';
+  locales = ['fr', 'en-GB'];
+
+  constructor(@Inject(LOCALE_ID) protected localeId: string) {
+    console.log('locale:', this.localeId); // "en-US"
+
+    this.locales.unshift(this.localeId);
+    this.locale = this.localeId;
+
+  }
+
+  updateLocal(event) {
+    this.locale = event.target.value;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { registerLocaleData } from '@angular/common';
+import localeFr from '@angular/common/locales/fr';
+import localeGb from '@angular/common/locales/en-GB';
+
+// the second parameter 'fr' is optional
+registerLocaleData(localeFr, 'fr');
+registerLocaleData(localeGb, 'en-GB');
 
 @NgModule({
   declarations: [
