664c9069b41bd980849acbfa032d65aaeefd3ef4
Jamie Perkins <jamie.perkins@gmail.com>
2021-04-23 06:52:52 -0600

    upgrade to angular 11

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,46 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
 
-// import locale data
-import { registerLocaleData } from '@angular/common';
-import localeFr from '@angular/common/locales/fr';
-import localeGb from '@angular/common/locales/en-GB';
-
-import { HttpClientModule } from '@angular/common/http';
-import { environment } from '../environments/environment';
-import { translocoLoader } from './transloco.loader';
-import { TranslocoModule, TRANSLOCO_CONFIG, TranslocoConfig } from '@ngneat/transloco';
-import { PipeDemoComponent } from './pipe-demo/pipe-demo.component';
 import { AppRoutingModule } from './app-routing.module';
-
-// register locales
-registerLocaleData(localeFr, 'fr');
-registerLocaleData(localeGb, 'en-GB');
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    PipeDemoComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule,
-    TranslocoModule,
-    AppRoutingModule,
+    AppRoutingModule
   ],
-  bootstrap: [AppComponent],
-  providers: [{
-      provide: TRANSLOCO_CONFIG,
-      useValue: {
-        listenToLangChange: true,
-        defaultLang: 'en',
-        fallbackLang: 'en',
-        prodMode: environment.production,
-        scopeStrategy: 'shared'
-      } as TranslocoConfig
-    },
-    translocoLoader
-  ]
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
