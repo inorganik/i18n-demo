55e41f585e385f02eff6e51448b18a5738c9ec85
Jamie Perkins <jamie.perkins@gmail.com>
2021-04-23 07:34:25 -0600

    re-add transloco

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+// import locale data
+import { registerLocaleData } from '@angular/common';
+import localeFr from '@angular/common/locales/fr';
+import localeGb from '@angular/common/locales/en-GB';
+
+import { HttpClientModule } from '@angular/common/http';
+import { TranslocoRootModule } from './transloco/transloco-root.module';
+import { PipeDemoComponent } from './pipe-demo/pipe-demo.component';
+import { AppRoutingModule } from './app-routing.module';
+
+// register locales
+registerLocaleData(localeFr, 'fr');
+registerLocaleData(localeGb, 'en-GB');
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PipeDemoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    TranslocoRootModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
